b254e92ba681856926e721f65a0856ae476f6f28
SusanaBaten <46938321+SusanaBaten@users.noreply.github.com>
2021-08-02 18:48:03 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+echo "# LabFinal" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SusanaBaten/LabFinal.git
+git push -u origin main
